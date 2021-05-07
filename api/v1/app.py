c1483cf53d35f64a9615986b28809ceb8067de36
torres1-23 <alejo.torres1247@gmail.com>
2021-05-07 19:32:15 +0000

    Fix pep8 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,6 +19,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r'/*': {'origins': '0.0.0.0'}})
 
+
 @app.teardown_appcontext
 def teardown(exc):
     """Teardown to remove the SQL session
