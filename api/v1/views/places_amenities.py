52690737a1d2b1de05f62e9e82eb5cf64821e729
torres1-23 <alejo.torres1247@gmail.com>
2021-05-10 23:56:25 +0000

    Add route amenities

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -4,7 +4,7 @@
 Usage:
     Add method to appviews blueprint to serve
     request to '/places/<place_id>/reviews' and
-    '/reviews' route"""
+    '/AMENITIES' route"""
 from api.v1.views import app_views
 from models import storage
 from flask import request, jsonify, abort
@@ -18,29 +18,26 @@ STORAGE = getenv('HBNB_TYPE_STORAGE')
 @app_views.route('/places/<place_id>/amenities',
                  methods=['GET'], strict_slashes=False)
 def amenities_by_places(place_id):
-    """Serve amenities route"""
     place_obj = storage.get(Place, place_id)
-    if request.method == 'GET':
-        if place_obj is None:
-            abort(404)
-        all_amenities = storage.all(Amenity)
-        if STORAGE == 'db':
-            place_amenities = place_obj.amenities
-        else:
-            place_amen_ids = place_obj.amenities
-            place_amenities = []
-            for amen in place_amen_ids:
-                response.append(storage.get(Amenity, amen))
-        place_amenities = [
-            obj.to_json() for obj in place_amenities
-            ]
-        return jsonify(place_amenities)
+    list_amenities = []
+    if place_obj is None:
+        abort(404)
+    if STORAGE == 'db':
+        amenities = storage.all(Amenity)
+        for obj in amenities:
+            list_amenities.append(obj.to_dict())
+    else:
+        amenities_id = place_obj.amenities
+        for id in amenities_id:
+            obj = storage.get(Amenity, id)
+            list_amenities.append(obj.to_dict())
+    return jsonify(list_amenities)
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  methods=['DELETE', 'POST'], strict_slashes=False)
 def amenity(place_id, amenity_id):
-    """Serve amenities route"""
+    """Serves amenities route"""
     place_obj = storage.get(Place, place_id)
     amenity_obj = storage.get(Amenity, amenity_id)
     if place_obj is None:
