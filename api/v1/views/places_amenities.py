af21af87aeb99011565efdc89944f1dba1c3314f
torres1-23 <alejo.torres1247@gmail.com>
2021-05-10 23:46:00 +0000

    Add route amenities

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -21,35 +21,37 @@ STORAGE = getenv('HBNB_TYPE_STORAGE')
 def amenities_by_places(place_id):
     """Serve amenities route"""
     place_obj = storage.get(Place, place_id)
-    list_amenities = []
-    if place_obj is None:
-        abort(404)
-    if STORAGE == 'db':
-        amenities = storage.all(Amenity)
-        for obj in amenities:
-            list_amenities.append(obj.to_json())
-    else:
-        amenities_id = place_obj.amenities
-        for id in amenities_id:
-            obj = storage.get(Amenity, id)
-            list_amenities.append(obj.to_dict())
-    return jsonify(list_amenities)
+    if request.method == 'GET':
+        if place_obj is None:
+            abort(404)
+        all_amenities = storage.all(Amenity)
+        if STORAGE == 'db':
+            place_amenities = place_obj.amenities
+        else:
+            place_amen_ids = place_obj.amenities
+            place_amenities = []
+            for amen in place_amen_ids:
+                response.append(storage.get(Amenity, amen))
+        place_amenities = [
+            obj.to_json() for obj in place_amenities
+            ]
+        return jsonify(place_amenities)
 
 
 @app_views.route('places/<place_id>/amenities/<amenity_id>',
                  methods=['DELETE', 'POST'], strict_slashes=False)
 def amenity(place_id, amenity_id):
     """Serve amenities route"""
-    place_obj = storage.get('Place', place_id)
-    amenity_obj = storage.get('Amenity', amenity_id)
+    place_obj = storage.get(Place, place_id)
+    amenity_obj = storage.get(Amenity, amenity_id)
     if place_obj is None:
-        abort(404, 'Not found')
+        abort(404)
     if amenity_obj is None:
-        abort(404, 'Not found')
+        abort(404)
     if request.method == 'DELETE':
         if (amenity_obj not in place_obj.amenities and
                 amenity_obj.id not in place_obj.amenities):
-            abort(404, 'Not found')
+            abort(404)
         if STORAGE == 'db':
             place_obj.amenities.remove(amenity_obj)
         else:
